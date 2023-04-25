453f13db17edff826d93fcac1c5a2b057c0b6022
tinabenita <tina1717rego@gmail.com>
2023-04-25 12:49:43 +0530

    index page done

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="indexcss.css">
+    <script src="https://kit.fontawesome.com/2c624fbb65.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <!--<script src="index.js"></script>-->
+  <nav>
+    <div class="navigate">
+      <ul class="left">
+        <li><a href="aboutus.php"><i style="color: black" class="fa-solid fa-house"></i> About Us</a></li>
+        <li><a href="user\usrlogin.php"><i style="color: black" class="fa-solid fa-user"></i> Login/Sign Up</a></li>
+        <li><a href="https://www.google.com/search?q=near+by+psychologists+and+psychiatrists&rlz=1C1ONGR_enIN976IN976&ei=bLBGZN3FKc3M2roP0rGouAo&oq=near+by+psychologists+and+psy&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAxgBMgUIIRCgATIFCCEQoAEyBQghEKABOgoIABBHENYEELADOg0IABBHENYEEMkDELADOgsIABCKBRCSAxCwAzoICAAQigUQkQI6BQgAEIAEOggIABAWEB4QCjoICAAQigUQhgM6BggAEBYQHjoICCEQFhAeEB06BwghEKABEApKBAhBGABQjAdY7zVgnUJoAXABeAGAAb8CiAGGDZIBCDEuMTEuMC4xmAEAoAEByAEJwAEB&sclient=gws-wiz-serp"><i style="color: black" class="fa-solid fa-hospital"></i> Near By Professionals</a></li>
+        <li><a href="contactus.php"><i style="color: black" class="fa-sharp fa-solid fa-people-group"></i>Team</a></li> 
+      </ul>
+    </div>
+  </nav>
+  <!--<div class="slideshow-container">
+    <img src="cat1.jpg">
+    <img src="cat2.jpg">
+    <img src="cat3.jpg">
+  </div>
+  <div class="dots-container">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+  </div>-->
+  
+  <!--<div class="backimg">
+    <img src="background1.jpg" alt="" width="1536px" height="675px">
+  </div>-->
+    
+     
+</body>
+</html>
