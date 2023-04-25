6d045f33243e83f7562430ed1f053a39cc4635be
tinabenita <tina1717rego@gmail.com>
2023-04-25 11:33:07 +0530

    contact page done

---
+++ contactus.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team</title>
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+    <!-- Google Font -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="cntcus.css">
+  </head>
+  <body>
+    <section>
+        <button  onclick="location.href='index.php'">Home</button>
+      <div class="row">
+        <h1>Our Team</h1>
+      </div>
+      <div class="row">
+        <!-- Column 1-->
+        <div class="column1">
+          <div class="card">
+            <div class="img-container">
+              <img src="tiniprofile.jpeg" />
+            </div>
+            <h3>Tina Benita Rego</h3>
+            <!--<p>Developer</p>-->
+            <!-- <h6>Full</h6> -->
+            <div class="icons">
+              <a href="https://www.linkedin.com/in/tina-benita-rego">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/tinabenita">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="mailto:20cs171.tina@sjec.ac.in">
+                <i class="fas fa-envelope"></i>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+        <!-- Column 2-->
+        <div class="column2">
+          <div class="card">
+            <div class="img-container">
+              <img src="vaneshaProfile.jpeg" />
+            </div>
+            <h3>Vanesha Relitsa Cutinho</h3>
+            <!--<p>Developer</p>-->
+            <div class="icons">
+              <a href="https://www.linkedin.com/in/vanesha-cutinho-37b29b255">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/VaneshaCutinho5">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="mailto:20cs174.vanesha@sjec.ac.in">
+                <i class="fas fa-envelope"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Column 3-->
+        <div class="column3">
+          <div class="card">
+            <div class="img-container">
+              <img src="sharanyaProfile.JPG" />
+            </div>
+            <h3>Sharanya S Shetty</h3>
+            <!--<p>Developer</p>-->
+            <!-- <h6>Full</h6> -->
+            <div class="icons">
+              <a href="https://www.linkedin.com/in/sharanya-shetty-154018207?trk=contact-info">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/sharanya2612">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="mailto:sharanyashetty2612@gmail.com">
+                <i class="fas fa-envelope"></i>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+        </div>
+    </section>
+  </body>
+</html>
