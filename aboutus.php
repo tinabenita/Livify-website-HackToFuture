97e7637555441d6928a4478015b7cfa8d4579c9c
tinabenita <tina1717rego@gmail.com>
2023-04-25 15:23:20 +0530

    aboutUs page done

---
+++ aboutus.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>About Us</title>
+    <link rel="stylesheet" type="text/css" href="about.css">
+  </head>
+  <body>
+    <button  onclick="location.href='index.php'">Home</button>
+    <div class="my-box frosted-glass-box">
+      <h2>About Us</h2>
+     
+      <p id="p1">Livify is a mental wellness website that offers a safe space for individuals to express their thoughts and feelings related to mental health. With the tagline "Where we speak the mind" Livify provides a platform for open discussions on mental health issues. Our goal is to create a community where individuals can find support, share their experiences, and learn from one another.</p>
+      <p id="p2">In addition to the community forum, Livify also provides a comprehensive list of mental health professionals based on the individual's area of residence. Our aim is to make it easy for individuals to access the care they need.</p>
+      <p id="p3">Moreover, Livify offers a stress-buster section that includes fun games, interesting articles, motivational videos and soft music to help individuals relieve stress. We believe in the power of self-care, and we strive to provide our users with the tools they need to take care of their mental health.</p>
+      <p id="p4">At Livify, we understand that mental health is a critical component of overall well-being. That's why we are committed to providing a safe, supportive, and inclusive space for individuals to prioritize their mental health.</p>
+    
+    </div>
+    
+  </body>
+</html>
